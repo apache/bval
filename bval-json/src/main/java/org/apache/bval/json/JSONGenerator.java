52d61e30dd6a39f14dfca393fcf8ee816197956d
Donald Woods <dwoods@apache.org>
2010-05-12 20:38:16 +0000

    BVAL-44 Move optional JSON code out of bval-core to a bval-json module, instead of creating a generic bval-optional module for now.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/bval/trunk@943674 13f79535-47bb-0310-9956-ffa450edef68

--- bval-json/src/main/java/org/apache/bval/json/JSONGenerator.java
+++ bval-json/src/main/java/org/apache/bval/json/JSONGenerator.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.bval.optional.json;
+package org.apache.bval.json;
 
 import java.io.IOException;
 import java.io.StringWriter;

22942d22f12bbc52ed5c198d2f83a715a8881a60
Simone Tripodi <simonetripodi@apache.org>
2012-01-07 01:05:31 +0000

    reorganizing stuff in different packages, according to covered area of interest

--- bval-extras/src/main/java/org/apache/bval/extras/constraints/net/InetAddressValidator.java
+++ bval-extras/src/main/java/org/apache/bval/extras/constraints/net/InetAddressValidator.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.bval.extras.constraints;
+package org.apache.bval.extras.constraints.net;
 
 import java.util.regex.Pattern;
 

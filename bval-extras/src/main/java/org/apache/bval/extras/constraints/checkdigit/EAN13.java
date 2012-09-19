1454242a63c1748d5bf73e96153d0523cb6c8b4c
romanstumm <romanstumm@13f79535-47bb-0310-9956-ffa450edef68>
2012-09-19 07:46:34 +0000

    fixed class comments
    git-svn-id: http://svn.apache.org/repos/asf/bval/trunk@1387476 13f79535-47bb-0310-9956-ffa450edef68

--- bval-extras/src/main/java/org/apache/bval/extras/constraints/checkdigit/EAN13.java
+++ bval-extras/src/main/java/org/apache/bval/extras/constraints/checkdigit/EAN13.java
@@ -37,7 +37,7 @@ import javax.validation.Payload;
  * as soon as a final version of the specification contains a similar functionality.
  * --
  * </p>
- * Description: annotation to validate a java.io.File is a directory<br/>
+ * Description: annotation to validate as EAN13<br/>
  */
 @Documented
 @Constraint( validatedBy = EAN13Validator.class )

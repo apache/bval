9f43dd36d8038946472d1b7ac907784efe5d459d
Matt Benson <mbenson@apache.org>
2018-02-24 13:56:37 -0600

    unused import

--- bval-jsr/src/main/java/org/apache/bval/jsr/metadata/Metas.java
+++ bval-jsr/src/main/java/org/apache/bval/jsr/metadata/Metas.java
@@ -32,7 +32,6 @@ import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Objects;
 

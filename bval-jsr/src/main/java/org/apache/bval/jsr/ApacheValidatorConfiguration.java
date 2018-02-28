6cb9a0b8d7a2e25b10b69031ae335e8bcfe40e8a
Matt Benson <mbenson@apache.org>
2018-02-28 11:29:57 -0600

    remove obsolete code

--- bval-jsr/src/main/java/org/apache/bval/jsr/ApacheValidatorConfiguration.java
+++ bval-jsr/src/main/java/org/apache/bval/jsr/ApacheValidatorConfiguration.java
@@ -19,7 +19,6 @@
 package org.apache.bval.jsr;
 
 import javax.validation.Configuration;
-import javax.validation.ValidatorContext;
 import javax.validation.ValidatorFactory;
 import javax.validation.spi.ConfigurationState;
 
@@ -40,57 +39,11 @@ public interface ApacheValidatorConfiguration extends Configuration<ApacheValida
         String VALIDATION_XML_PATH = "apache.bval.validation-xml-path";
 
         /**
-         * true/false. use Introspector (java beans) metadata additionally
-         * to build metadata with JSR303.<br>
-         * This means that all properties exist that are java-bean properties and
-         * and that some features (Hidden, Readonly) are taken from Introspector
-         * to create the meta data.<br>
-         * default: false
-         */
-        String ENABLE_INTROSPECTOR = "apache.bval.enable-introspector";
-
-        /**
-         * true/false. use Apache metaBeans xml format additionally to
-         * build metadata with JSR303.
-         * default: false
-         *
-         * @deprecated we could decide to drop this feature in the future.
-         * we keep it as long as we support both: jsr and xstream-xml meta data at
-         * the same time (and potentially for the same domain classes)
-         */
-        @Deprecated
-        String ENABLE_METABEANS_XML = "apache.bval.enable-metabeans-xml";
-
-        /**
-         * - true (validate maps like beans, so that
-         *     you can use Maps to validate dynamic classes or
-         *     beans for which you have the MetaBean but no instances)
-         * - false (default), validate maps like collections
-         *     (validating the values only)
-         * default: false
-         */
-        String TREAT_MAPS_LIKE_BEANS = "apache.bval.treat-maps-like-beans";
-
-        /**
          * Specifies the classname of the {@link ValidatorFactory} to use: this
          * class is presumed have a constructor that accepts a single
          * {@link ConfigurationState} argument.
          */
         String VALIDATOR_FACTORY_CLASSNAME = "apache.bval.validator-factory-classname";
-
-        /**
-         * Specifies the names, delimited by whitespace, of
-         * {@link MetaBeanFactory} classes that should be added to collaborate
-         * with an {@link ApacheFactoryContext}'s {@link MetaBeanFinder}. These
-         * are instantiated per {@link ValidatorContext}, attempting to use
-         * constructor arguments of decreasing specificity:
-         * <ol>
-         * <li>assignable from the creating {@link ApacheFactoryContext}</li>
-         * <li>assignable from the associated {@link ApacheValidatorFactory}</li>
-         * <li>default (no-args) constructor</li>
-         * </ol>
-         */
-        String METABEAN_FACTORY_CLASSNAMES = "apache.bval.metabean-factory-classnames";
 
         /**
          * Size to use for caching of constraint-related information. Default is {@code 50}.

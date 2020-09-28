29e2905d6027a525860178f93d541bb443a1f257
Jean-Louis Monteiro <jlmonteiro@tomitribe.com>
2020-09-28 14:29:59 +0200

    Refactor to move microbenchmark to a separate module

--- bval-perf/src/test/java/org/apache/bval/jsr/Jsr303Benchmark.java
+++ bval-perf/src/test/java/org/apache/bval/jsr/Jsr303Benchmark.java
@@ -27,7 +27,6 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.profile.JavaFlightRecorderProfiler;
-import org.openjdk.jmh.profile.Profiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
